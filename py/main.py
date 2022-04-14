63fd80dd89ab8fd55c0f547d74b0002c4f6c0193
kremlev404 <kremlev404@gmail.com>
2022-04-14 13:33:37 -0700

    added py script to update Firebase storage

---
+++ py/main.py
@@ -0,0 +1,20 @@
+import pyrebase
+
+firebaseConfig = {
+    'apiKey': "AIzaSyDNjvqZU1sgyycaNAUKsuNKNERqYqTohMo",
+    'authDomain': "ncsrecognition.firebaseapp.com",
+    'databaseURL': "https://ncsrecognition-default-rtdb.europe-west1.firebasedatabase.app",
+    'projectId': "ncsrecognition",
+    'storageBucket': "ncsrecognition.appspot.com",
+    'messagingSenderId': "311133578336",
+    'appId': "1:311133578336:web:24c82cf92cc51e918a4f05",
+    'measurementId': "G-4VZCTGE7M4"
+}
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    db = firebase.database()
+    db.child("kremlev404").child("raspId1").update({'type': "raspberry"})
+    db.child("kremlev404").child("raspId1").child("recognizedPersonId1").update(
+        {'prob': [0.70, 0.22, 0.77, 0.65],
+         'timestamp': ["14.04.2022;23.00", "14.04.2022;23.05", "14.04.2022;23.10", "14.04.2022;23.15"]})
